a76f29d78f5c36fa150bf26521063df0e6bc1993
juliet oti <egwuotijulietchinenye@gmail.com>
2022-12-24 01:35:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  let billRef = useRef();
+  let tipRef = useRef();
+   let tipAmtRef = useRef();
+  let [num, setNum] = useState({ tipAmt: 0, total: 0 });
+  let calculate = () => {
+    let billvalue = billRef.current.value;
+    let tipvalue = tipRef.current.value;
+    console.log(billvalue);
+    console.log(tipvalue);
+    let result = (tipvalue / 100) * billvalue;
+    console.log(result);
+    let totalbill = Number(result) + Number(billvalue);
+    console.log(totalbill);
+    setNum({ tipAmt: result, total: totalbill });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-con">
+        <h2>Tip Calculator</h2>
+        <div className="input-con">
+          <div>
+            <label htmlFor="bill-amount">Bill Amount:</label>
+            <input ref={billRef} type="text" />
+          </div>
+          <div>
+            <label htmlFor="percent">Percentage Tip:</label>
+            <input placeholder="%" ref={tipRef} type="text" />
+          </div>
+          <div>
+            <label htmlFor="bill-amount">Tip Amount:</label>
+            <input disabled ref={tipAmtRef} type="text" placeholder={ "$" + num.tipAmt} />
+          </div>
+          <div>
+            <label htmlFor="bill-amount">Total:</label>
+            <input  disabled type="text" placeholder={"$" + num.total} />
+          </div>
+          <button onClick={calculate}>Calculate</button>
+        </div>
+      </div>
     </div>
   );
 }
